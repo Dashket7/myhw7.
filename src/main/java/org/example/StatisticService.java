dde7f48d95bb9c28abdb78c62ea1e7404d9eb4da
Artemeva D <dashartemeva7@gmail.com>
2023-08-01 10:51:40 +0500

    second commit

---
+++ src/main/java/org/example/StatisticService.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class StatisticService {
+    public long findMax(long[] incomes) {
+        long currentMax = incomes[0];
+        for (long income : incomes) {
+            if (currentMax < income) {
+                currentMax = income;
+            }
+        }
+        return currentMax;
+    }
+    }
---
+++ src/test/java/org/example/StatisticsServiceTest.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class StatisticsServiceTest {
+    @Test
+
+   public void findMax() {
+        StatisticService service = new StatisticService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
+
